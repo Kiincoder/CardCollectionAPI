a4a275f4926f1d1b6cc44089c41f56f5278962fa
kiincoder <alemaoazedo2@gmail.com>
2025-08-05 02:16:06 -0300

    [ADD] New routes

--- routes/criarsessao.py
+++ routes/criarsessao.py
@@ -1,28 +1,28 @@
 from flask import Blueprint, jsonify, request
 import jwt
 from datetime import datetime, timedelta
-from app import app
 from models.models import usuarios
+from connection.config import Config
 
 criarsessao_bp = Blueprint('criarsessao', __name__)
 
-@criarsessao_bp.route('api/', methods=["POST"])
+@criarsessao_bp.route('/', methods=["POST"])
 def criarsessao():
-  response = request.json
-  email = response.get('email')
-  senha = response.get('senha')
+  data = request.get_json()
+  email = data['email']
+  senha = data['senha']
 
   if not email or not senha:
     return jsonify({'erro': 'Email e senha são obrigatórios'}), 400
   
-  usuario = usuarios.query.filter_by(email=email).first()
+  usuario = usuarios.query.filter_by(email).first()
 
-  if usuario and usuarios.verificar_senha(senha_plana=senha):
+  if usuario and usuarios.verificar_senha(senha, usuario.senha):
     payload = {
-        'user_id': usuario.id,
+        'user_id': usuario.uid,
         'exp': datetime.utcnow() + timedelta(hours=2) 
     }
-    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
-    return jsonify({'token': token})
+    token = jwt.encode(payload, Config.SECRET_KEY, algorithm='HS256')
+    return jsonify({'token': token}), 200
   else:
     return jsonify({'erro': 'Credenciais inválidas'}), 401
--- routes/criarusuario.py
+++ routes/criarusuario.py
@@ -1,9 +1,23 @@
 from flask import Blueprint, jsonify, request
-from app import app
-
+from models.models import usuarios
+from models.models import db
 
 criarusuario_bp = Blueprint('criarusuario', __name__)
 
-@criarusuario_bp.route('api/', methods=["POST"])
+@criarusuario_bp.route('/', methods=["POST"])
 def criarusuario():
-  return jsonify({'message': 'oi'}), 200
+  data = request.get_json()
+  if not data or not all(k in data for k in ('email', 'senha', 'nome')):
+    return jsonify({'mensagem': 'Dados incompletos!'}), 400
+  
+  if usuarios.query.filter_by(email=data['email']).first():
+    return jsonify({'mensagem': 'Email já cadastrado!'}), 400
+  
+  usuario = usuarios(
+    email= data['email'],
+    senha= usuarios.encrypt_senha(data['senha']),
+    nome = data['nome']  
+  )
+  db.session.add(usuario)
+  db.session.commit()
+  return jsonify({'mensagem': 'Usuário criado com sucesso!'}), 200
